4b88db3c482e101f90d6c3ed0cefcf225eabbf9b
adsilva <adesh.dsilva@yahooinc.com>
2024-03-05 13:27:06 +0000

    complete - 1

--- script.js
+++ script.js
@@ -1,69 +1,80 @@
-window.onload = function() {
+window.onload = function () {
 
+    $('body')
+        .css('transition', 'background-color linear 3s')
+        .css('background-color', '#2d1b55')
+        .on('transitionend', function () {
+            //$('#birthday-message').show().on('animationend', rest);
+            $('.container').fadeIn('fast', start);
+        });
 
-    new TypeIt("#app", {
-        speed: 20,
-        loop: false,
-        afterComplete: async (instance) => {
-            new TypeIt("#que", {
-                strings: "I wanted to <span style=\"color: #27ae60;\">wish</span> you...",
-                speed: 150,
-                loop: false,
-            })
-                .go()
-        },
-    })
-        .type("Hi Riyu &#128075;")
-        .break()
-        .pause(500)
-        .type("Today is an amazing day!")
-        .go();
-
-
-/*        .deleteChars(10)
-        .typeString('<strong>JS</strong> plugin for a cool typewriter effect and ')
-        .typeString('<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>')
-        .pauseFor(1000)*/
-
+    $('.loading').fadeOut('fast');
+    //$('.container').fadeIn('fast', start);
 }
 
 
 
-function startQuestions() {
-    document.getElementById("questions").style.display = "block";
-  }
-  
-  function answerYes() {
-    document.getElementById("questions").style.display = "none";
-      document.getElementById("que").style.display = "none";
-      document.getElementById("init").innerText = "YEY YEY 🎉🎉🎉";
-    document.getElementById("message").style.display = "block";
-    document.getElementById("messageText").innerText = "I believe in love at first sight too! How about we go on a date and see if it's true?";
-    showConfetti()
+var start = function () {
+
+    setTimeout(() => {
+
+        new TypeIt("#app", {
+            speed: 200,
+            loop: false,
+            afterComplete: async (instance) => {
+                $('#app .ti-cursor').remove()
+                new TypeIt("#que", {
+                    strings: "I wanted to <span style=\"color: #27ae60;\">wish</span> you...",
+                    speed: 200,
+                    loop: false,
+                    afterComplete: wishBirthday
+                })
+                    .go()
+            },
+        })
+            .type("Hi Riyu &#128075;")
+            .break()
+            .pause(500)
+            .type("Today is an amazing day!")
+            .go();
+
+
+    }, 300);
 }
-  
-  function answerNo() {
-    document.getElementById("questions").style.display = "none";
-    document.getElementById("que").style.display = "none";
-    document.getElementById("init").innerText = "YEY YEY 🎉🎉🎉";
-    document.getElementById("message").style.display = "block";
-    document.getElementById("messageText").innerText = "No worries, love takes time. But can we spend some time together and see if something beautiful blossoms?";
-    showConfetti()
+
+var wishBirthday = function () {
+
+    $('#messagePhase1').fadeOut('fast', function () {
+
+
+        $('body')
+            .css('transition', 'background-color linear 3s')
+            .css('background-color', '#a27bcc')
+            .on('transitionend', function () {
+                //$('#birthday-message').show().on('animationend', rest);
+                $('.cake').fadeIn('slow', function () {
+                    $('#birthday-message').show()
+                    showConfetti();
+                });
+            });
+    });
 
 }
-  
+
+
 // Function to show confetti
 function showConfetti() {
     const confettiSettings = {
-      target: 'confetti-canvas',
-      respawn: true,
-      colors: ['#ff0000', '#00ff00', '#0000ff'], 
+        target: 'confetti-canvas',
+        respawn: true,
+        colors: ['#ff0000', '#00ff00', '#0000ff'],
     };
-  
+
     const confetti = new ConfettiGenerator(confettiSettings);
     confetti.render();
-    setTimeout(() => {
-      confetti.clear();
-    }, 5000);
-  }
+    /*setTimeout(() => {
+        confetti.clear();
+    }, 5000);*/
+
+}
   
